ff8936bc65a77721de34fa343a3bc3c7d3c4c14a
Sishaar Rao <sishaarrao@Sishaars-MacBook-Pro-Lobcom.local>
2020-02-25 19:52:45 -0800

    feat(database): created database and JS libraries for interacting with tables

--- config/test.js
+++ config/test.js
@@ -6,5 +6,9 @@ module.exports = {
   DB_NAME: 'sfmovies_test',
   DB_PASSWORD: '',
   DB_PORT: 5432,
+<<<<<<< HEAD
   DB_USER: 'sfmovies_user'
+=======
+  DB_USER: 'sfmovies_user' 
+>>>>>>> feat(database): created database and JS libraries for interacting with tables
 };
