c5bff8d9ffffcf024a357e31dc33df720e85b3b9
Sishaar Rao <sishaarrao@Sishaars-MacBook-Pro-Lobcom.local>
2020-03-02 13:33:20 -0800

    linting errors

--- db/migrations/20200302112659_drop_title_from_movies.js
+++ db/migrations/20200302112659_drop_title_from_movies.js
@@ -5,10 +5,10 @@ exports.up = (Knex) => {
     table.dropColumn('title');
   })
   .then(() => {
-		return Knex.raw('ALTER TABLE movies ADD CONSTRAINT movies_name_not_null CHECK (name IS NOT NULL) NOT VALID')
-		.then(() => {
-			return Knex.raw('ALTER TABLE movies VALIDATE CONSTRAINT movies_name_not_null');
-		});
+    return Knex.raw('ALTER TABLE movies ADD CONSTRAINT movies_name_not_null CHECK (name IS NOT NULL) NOT VALID')
+    .then(() => {
+      return Knex.raw('ALTER TABLE movies VALIDATE CONSTRAINT movies_name_not_null');
+    });
   });
 };
 
