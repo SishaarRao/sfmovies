f865cf0a3230b0cbab1b0cd4531d541c5931c159
Sishaar Rao <sishaarrao@Sishaars-MacBook-Pro-Lobcom.local>
2020-03-04 13:42:08 -0800

    added test for if other parameters is passed in

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -6,12 +6,22 @@ describe('movie controller', () => {
 
   describe('create', () => {
 
-    it('creates a movie', async () => {
+    it('creates a movie using title', async () => {
       const payload = { title: 'WALL-E' };
 
       const movie = await Controller.create(payload);
 
       expect(movie.get('title')).to.eql(payload.title);
+      expect(movie.get('name')).to.eql(payload.name);
+    });
+
+    it('creates a movie using name', async () => {
+      const payload = { name: 'WALL-E' };
+
+      const movie = await Controller.create(payload);
+
+      expect(movie.get('title')).to.eql(payload.title);
+      expect(movie.get('name')).to.eql(payload.name);
     });
 
   });
