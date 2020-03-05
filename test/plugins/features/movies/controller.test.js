b18af811751f053b266df084d7c3add973cebb1e
Sishaar Rao <sishaarrao@Sishaars-MacBook-Pro-Lobcom.local>
2020-03-05 09:57:47 -0800

    drop support for title

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -6,21 +6,11 @@ describe('movie controller', () => {
 
   describe('create', () => {
 
-    it('creates a movie using title', async () => {
-      const payload = { title: 'WALL-E' };
-
-      const movie = await Controller.create(payload);
-
-      expect(movie.get('title')).to.eql(payload.title);
-      expect(movie.get('name')).to.eql(payload.name);
-    });
-
     it('creates a movie using name', async () => {
       const payload = { name: 'WALL-E' };
 
       const movie = await Controller.create(payload);
 
-      expect(movie.get('title')).to.eql(payload.title);
       expect(movie.get('name')).to.eql(payload.name);
     });
 
