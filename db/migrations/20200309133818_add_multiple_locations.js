426143f286e6dc35404cc7f73c846fef76557744
Sishaar Rao <sishaarrao@Sishaars-MacBook-Pro-Lobcom.local>
2020-03-09 14:28:56 -0700

    second step in migration

--- db/migrations/20200309133818_add_multiple_locations.js
+++ db/migrations/20200309133818_add_multiple_locations.js
@@ -2,14 +2,13 @@
 
 const TABLE = 'movies';
 const COLUMN_NAME = 'locations';
-// const DEFAULT_LOCATION = 'San Francisco';
+const DEFAULT_LOCATION = 'San Francisco';
 
 exports.up = async (knex) => {
   await knex.schema.table(TABLE, (table) => {
     table.specificType(COLUMN_NAME, 'text[]');
   });
-  // Set default value, will be done in second step
-  // await knex.raw(`ALTER TABLE ${TABLE} ALTER COLUMN ${COLUMN_NAME} SET DEFAULT '{ "${DEFAULT_LOCATION}" }' `);
+  await knex.raw(`ALTER TABLE ${TABLE} ALTER COLUMN ${COLUMN_NAME} SET DEFAULT '{ "${DEFAULT_LOCATION}" }' `);
 };
 
 exports.down = async (knex) => {
