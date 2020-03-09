8e3c3497dd93486cf2aba469cc55fca2f1b4dbd8
Sishaar Rao <sishaar@gmail.com>
2020-03-09 13:27:39 -0700

    feat(route): GET /movies with filters
    Overview:
    
    retrieve a list of movies
    filter by exact release year or a range of years
    filter by exact or fuzzy title
    Details per bullet:
    
    Either the parameter release_year can be specified OR either one or both of start_year and release_year. If the range option is selected but only start_year is specified, then results will be movies created following the start_year. If only end_year is specified, then results will be movies created before the end_year. If both start_year and end_year parameters are specified, then results will be within the range of those years.
    e.g.
    GET/movies?release_year=1980 : Get all movies released in 1980
    GET/movies?start_year=1980 : Get all movies released after and including 1980
    GET/movies?end_year=1980 : Get all movies released before and including 1980
    GET/movies?start_year=1970&end_year=1980 : Get all movies released between and including 1970 to 1980
    
    Movie can be exactly equal to name, or a fuzzy title if the parameter similar is set to true. If this is the case, then results will include all movie names with the parameter name appearing within it.
    e.g.
    GET/movies?name=Alcatraz : Get all movies that are named Alcatraz (Alcatraz - 2012)
    GET/movies?name=Alcatraz&similar=true : Get all movies with titles related to Alcatraz (Alcatraz - 2012, Birdman of Alcatraz - 1962, Escape from Alcatraz - 1979)

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -16,4 +16,52 @@ describe('movie controller', () => {
 
   });
 
+  describe('retrieve', () => {
+
+    it('retrieves a list of movies by name', async () => {
+      const payload = { name: 'Alcatraz' };
+
+      const movie = await Controller.retrieve(payload);
+
+      for (let i = 0; i < movie.length; i++) {
+        expect(movie[i].attributes.name).to.eql(payload.name);
+      }
+    });
+
+    it('retrieves a list of movies by a fuzzy name', async () => {
+      const payload = { name: 'Alcatraz', similar: 'true' };
+
+      const movie = await Controller.retrieve(payload);
+
+      expect(movie.length).to.eql(3);
+      for (let i = 0; i < movie.length; i++) {
+        expect(movie[i].attributes.name).to.include(payload.name);
+      }
+    });
+
+    it('retrieves a list of movies by release_year', async () => {
+      const payload = { release_year: 1980 };
+
+      const movie = await Controller.retrieve(payload);
+
+      expect(movie.length).to.eql(5);
+      for (let i = 0; i < movie.length; i++) {
+        expect(movie[i].attributes.release_year).to.eql(payload.release_year);
+      }
+    });
+
+    it('retrieves a list of movies by range of years', async () => {
+      const payload = { start_year: 1970, end_year: 1980 };
+
+      const movie = await Controller.retrieve(payload);
+
+      expect(movie.length).to.eql(30);
+      for (let i = 0; i < movie.length; i++) {
+        expect(movie[i].attributes.release_year).to.be.at.least(payload.start_year);
+        expect(movie[i].attributes.release_year).to.be.at.most(payload.end_year);
+      }
+    });
+
+  });
+
 });
